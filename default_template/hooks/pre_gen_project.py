8213883d18e413beb142a952809967a3d74be627
John Hoffmann <johnvonhoffmann@gmail.com>
2023-11-16 08:17:26 +0100

    check if print width is integer

--- default_template/hooks/pre_gen_project.py
+++ default_template/hooks/pre_gen_project.py
@@ -1,1 +1,6 @@
 assert "\\" not in "{{ cookiecutter.author_name }}", "Don't include backslashes in author name."
+
+code_formatter_print_width = "{{ cookiecutter.code_formatter_print_width }}"
+assert (
+    code_formatter_print_width.isdigit() and float(code_formatter_print_width).is_integer()
+), "Invalid value for code_formatter_print_width. Please provide an integer value."
